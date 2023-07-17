7601c3b9924ef24cfd28b2957b3ed4608aa5664a
Shubham <shubhamsikarwar43@gmail.com>
2023-07-17 23:54:32 +0530

    Initial Commits

---
+++ pickbook_bk/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(1234,()=>{
+    console.log('Server is running on port 1234')
+})
