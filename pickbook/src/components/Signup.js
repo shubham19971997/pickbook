5a48b475509f44e8db57228bba786c0ff420c9ae
Shubham <shubhamsikarwar43@gmail.com>
2023-07-25 03:27:53 +0530

    building signup page

--- pickbook/src/components/Signup.js
+++ pickbook/src/components/Signup.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='flex'>
+      <div className='w-screen'>
+
+      </div>
+      <div className='w-screen h-screen bg-slate-100 hover:blur-2xl ease-out duration-200 flex justify-center items-center' >
+          <div className='w-56 h-56 rounded-full bg-violet-700'>
+          
+          </div>
+      </div>
+    </div>
   )
 }
 
