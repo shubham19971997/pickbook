7eb21c0bf154a418cb725d90c63eb8e8979b377d
Shubham <shubhamsikarwar43@gmail.com>
2023-08-01 11:37:09 +0530

    configuring db connection

--- pickbook_bk/db/userSchema.js
+++ pickbook_bk/db/userSchema.js
@@ -29,4 +29,4 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
-export default User;
+module.exports = User;
--- pickbook_bk/server.js
+++ pickbook_bk/server.js
@@ -1,36 +1,42 @@
 const express = require("express");
-const { MongoClient,ServerApiVersion } = require("mongodb");
 require('dotenv').config();
-
+const mongoose = require('mongoose');
 
 
 const app = express();
 const uri = process.env.URI;
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
+mongoose.connect(uri, {useNewUrlParser: true})
+
+const conn = mongoose.connection
+
+conn.on('open', () => {
+  console.log('Connected to DB...')
+})
 
 //DB connection
-let db;
-async function run() {
-  try {
-    await client.connect();
-    db = client.db("pickbook")
-    
-    console.log(
-      "Connected to DB."
-    );
-  } catch (err) {
-    console.log(err);
-  }
-}
-run().catch(console.dir);
-
+// let conn;
+// async function run() {
+//   try {
+//     conn = await client.connect(); 
+//     console.log(conn.db)
+//     console.log(conn)
+//     console.log(
+//       "Connected to DB."
+//     );
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+// run().catch(console.dir);
 
 const getRoute = require('./routes/getRoutes');
 app.use('/',getRoute)
@@ -38,9 +44,3 @@ app.use('/',getRoute)
 app.listen(1234, () => {
   console.log("Server is running on port 1234");
 });
-
-
-//exports
-module.exports={
-  db,app
-}
