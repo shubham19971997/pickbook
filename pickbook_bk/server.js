882ea77f1850d74b94db2106da83f53bded62b10
Shubham <shubhamsikarwar43@gmail.com>
2023-08-02 12:30:45 +0530

    configuring db connection

--- pickbook_bk/server.js
+++ pickbook_bk/server.js
@@ -1,45 +1,23 @@
 const express = require("express");
-require('dotenv').config();
-const mongoose = require('mongoose');
-
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 const uri = process.env.URI;
 
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose.connect(uri, {useNewUrlParser: true})
+const conn = mongoose.connection;
 
-const conn = mongoose.connection
+conn.on("open", () => {
+  console.log("Connected to DB...");
+});
 
-conn.on('open', () => {
-  console.log('Connected to DB...')
-})
-
-//DB connection
-// let conn;
-// async function run() {
-//   try {
-//     conn = await client.connect(); 
-//     console.log(conn.db)
-//     console.log(conn)
-//     console.log(
-//       "Connected to DB."
-//     );
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-// run().catch(console.dir);
-
-const getRoute = require('./routes/getRoutes');
-app.use('/',getRoute)
+const getRoute = require("./routes/getRoutes");
+app.use("/", getRoute);
 
 app.listen(1234, () => {
   console.log("Server is running on port 1234");
